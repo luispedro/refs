7c9cc7625d7d43fc31fce37e02318655280e014d
Luís Pedro Coelho <lpc@cmu.edu>
2009-05-07 11:54:10 -0400

    Fix readpdf tests

--- tests/test_readpdf.py
+++ tests/test_readpdf.py
@@ -3,11 +3,11 @@ import refs.readpdf
 _example_file = 'tests/data/re_manifold.pdf'
 
 def test_geturi():
-    uri = refs.readpdf.md5sum('_example_file')
+    uri = refs.readpdf.geturi(_example_file)
     assert uri.startswith('bib')
 
 def test_md5sum():
-    assert refs.readpdf.md5sum('_example_file') != refs.readpdf.md5sum('_example_file', include_size=True)
-    assert refs.readpdf.md5sum('_example_file') == refs.readpdf.md5sum('_example_file', include_size=False)
-    assert refs.readpdf.md5sum('_example_file') != refs.readpdf.md5sum('_example_file', 122)
+    assert refs.readpdf.md5sum(_example_file) != refs.readpdf.md5sum(_example_file, include_size=True)
+    assert refs.readpdf.md5sum(_example_file) == refs.readpdf.md5sum(_example_file, include_size=False)
+    assert refs.readpdf.md5sum(_example_file) != refs.readpdf.md5sum(_example_file, 122)
 
